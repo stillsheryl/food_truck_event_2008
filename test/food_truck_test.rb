7ec91885669d8e6a203ee91f81d966434e22033e
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-15 13:28:44 -0600

    Add test can check inventory after adding stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -37,4 +37,21 @@ class FoodTruckTest < Minitest::Test
     assert_equal expected, food_truck.inventory
   end
 
+  def test_can_check_inventory_after_stocking
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    food_truck.stock(item1, 30)
+
+    assert_equal 30, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 25)
+    assert_equal 55, food_truck.check_stock(item1)
+
+    food_truck.stock(item2, 12)
+
+    expected = {item1=> 55,item2=> 12}
+    assert_equal expected, food_truck.inventory
+  end
+
 end
