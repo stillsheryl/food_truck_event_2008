801044f929dddfc58d496ddeedf074f83ec1cfbe
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-15 13:51:49 -0600

    Add food trucks that sell item test

--- test/event_test.rb
+++ test/event_test.rb
@@ -61,4 +61,26 @@ class EventTest < Minitest::Test
     assert_equal ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"], event.food_truck_names
   end
 
+  def test_food_trucks_that_sell
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    assert_equal [food_truck1, food_truck3], event.food_trucks_that_sell(item1)
+    assert_equal [food_truck2], event.food_trucks_that_sell(item4)
+  end
+
 end
