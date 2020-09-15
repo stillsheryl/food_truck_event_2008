ed1b04f7abd616db721e1be07e6fc6b3ee72bcd8
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-15 13:20:21 -0600

    Add test can stock items

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -27,4 +27,14 @@ class FoodTruckTest < Minitest::Test
     assert_equal 0, food_truck.check_stock(item1)
   end
 
+  def test_can_stock_items
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    food_truck.stock(item1, 30)
+
+    expected = {item1 => 30}
+    assert_equal expected, food_truck.inventory
+  end
+
 end
